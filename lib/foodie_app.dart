66a3b0189918ed65e37df570db219a424fbf0379
youssefkhaled <youssefwahdan12@gmail.com>
2025-06-26 20:13:12 +0300

    - Added the neccessary packages - Created file for the project - Generated localization files

---
+++ lib/foodie_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FoodieApp extends StatelessWidget {
+  const FoodieApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
